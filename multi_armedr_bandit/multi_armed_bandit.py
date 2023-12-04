70353b48bc8f19a43e75c71726f971faaaec0648
Harrbit <70574504+Harrbit@users.noreply.github.com>
2023-12-04 20:29:54 +0800

    搞实业搞实业搞实业！ an implementation of the epsilon-greedy algorithm and some extended exploration

--- multi_armedr_bandit/multi_armed_bandit.py
+++ multi_armedr_bandit/multi_armed_bandit.py
@@ -78,3 +78,11 @@ epsilon_greedy_solver = EpsilonGreedy(bandit_10_arms, epsilon=0.01)
 epsilon_greedy_solver.run(5000)
 print(epsilon_greedy_solver.regret)
 plot_results([epsilon_greedy_solver], ["epsilon greedy"])
+
+np.random.seed(0)
+epsilons = [1e-4, 0.01, 0.1, 0.25, 0.5]
+epsilon_greedy_solver_list = [EpsilonGreedy(bandit_10_arms, epsilon=e) for e in epsilons]
+epsilon_greedy_solver_names = ["epsilon = {}".format(e) for e in epsilons]
+for solver in epsilon_greedy_solver_list:
+    solver.run(5000)
+plot_results(epsilon_greedy_solver_list, epsilon_greedy_solver_names)
