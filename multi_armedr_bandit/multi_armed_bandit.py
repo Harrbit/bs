e2678c42af1d555247861c5d578635428e188e70
Harrbit <70574504+Harrbit@users.noreply.github.com>
2023-12-04 20:11:15 +0800

    搞实业搞实业搞实业！ an implementation of the epsilon-greedy algorithm

--- multi_armedr_bandit/multi_armed_bandit.py
+++ multi_armedr_bandit/multi_armed_bandit.py
@@ -1,27 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 '''理想状态下的多臂老虎机问题解决方法'''
 
 
 class BernouliBandit:
-    def __init__(self, k):
-        self.prob = np.random.uniform(size=k)
-        self.best_idx = np.argmax(self.prob)
-        self.best_prob = self.prob[self.best_idx]
-        self.k = k
+    def __init__(self, K):
+        self.probs = np.random.uniform(size=K)
+        self.best_idx = np.argmax(self.probs)
+        self.best_prob = self.probs[self.best_idx]
+        self.K = K
     
     def step(self, k):
-        if np.random.rand < self.prob[k]:
+        if np.random.rand() < self.probs[k]:
             return 1
         else:
             return 0
 
-'''
+
 np.random.seed(1)
-K = 10000
+K = 10
 bandit_10_arms = BernouliBandit(K)
 print("%d,%.4f" % (bandit_10_arms.best_idx, bandit_10_arms.best_prob))
-'''
-class solver:
+
+
+class Solver:
     def __init__(self, bandit):
         self.bandit = bandit
         self.counts = np.zeros(self.bandit.K)
@@ -29,7 +31,7 @@ class solver:
         self.action = []
         self.regrets = []
 
-    def update_regret(self,k):
+    def update_regret(self, k):
         self.regret += self.bandit.best_prob - self.bandit.probs[k]
         self.regrets.append(self.regret)
 
@@ -42,3 +44,37 @@ class solver:
             self.counts[k] += 1
             self.action.append(k)
             self.update_regret(k)
+
+
+class EpsilonGreedy(Solver):
+    def __init__(self, bandit, epsilon=0.01, init_prob=1.0):
+        super(EpsilonGreedy, self).__init__(bandit)
+        self.epsilon = epsilon
+        self.estimates = np.array([init_prob] * self.bandit.K)
+
+    def run_one_step(self):
+        if np.random.random() < self.epsilon:
+            k = np.random.randint(0, self.bandit.K)
+        else:
+            k = np.argmax(self.estimates)
+        r = self.bandit.step(k)
+        self.estimates[k] += 1./(self.counts[k] + 1) * (r - self.estimates[k])
+        return k
+
+
+def plot_results(solvers, solver_names):
+    for idx, solver in enumerate(solvers):
+        time_list = range(len(solver.regrets))
+        plt.plot(time_list, solver.regrets, label=solver_names[idx])
+    plt.xlabel('Time steps')
+    plt.ylabel('Cumulative regrets')
+    plt.title('%d-armed bandit' % solvers[0].bandit.K)
+    plt.legend()
+    plt.show()
+
+
+np.random.seed(1)
+epsilon_greedy_solver = EpsilonGreedy(bandit_10_arms, epsilon=0.01)
+epsilon_greedy_solver.run(5000)
+print(epsilon_greedy_solver.regret)
+plot_results([epsilon_greedy_solver], ["epsilon greedy"])
