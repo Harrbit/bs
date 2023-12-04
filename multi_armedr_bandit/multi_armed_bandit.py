17b80a5c96b4a7017b2d6d9b1dd858e4e2af7979
Harrbit <70574504+Harrbit@users.noreply.github.com>
2023-12-04 11:44:45 +0800

    a structure for further implement

--- multi_armedr_bandit/multi_armed_bandit.py
+++ multi_armedr_bandit/multi_armed_bandit.py
@@ -1,7 +1,9 @@
 import numpy as np
 '''理想状态下的多臂老虎机问题解决方法'''
+
+
 class BernouliBandit:
-    def __init__(self,k):
+    def __init__(self, k):
         self.prob = np.random.uniform(size=k)
         self.best_idx = np.argmax(self.prob)
         self.best_prob = self.prob[self.best_idx]
@@ -13,7 +15,30 @@ class BernouliBandit:
         else:
             return 0
 
+'''
 np.random.seed(1)
-k = 1000
-bandit_10_arms=BernouliBandit(k)
-print("%d,%.4f"%(bandit_10_arms.best_idx,bandit_10_arms.best_prob))
+K = 10000
+bandit_10_arms = BernouliBandit(K)
+print("%d,%.4f" % (bandit_10_arms.best_idx, bandit_10_arms.best_prob))
+'''
+class solver:
+    def __init__(self, bandit):
+        self.bandit = bandit
+        self.counts = np.zeros(self.bandit.K)
+        self.regret = 0
+        self.action = []
+        self.regrets = []
+
+    def update_regret(self,k):
+        self.regret += self.bandit.best_prob - self.bandit.probs[k]
+        self.regrets.append(self.regret)
+
+    def run_one_step(self):
+        raise NotImplementedError
+
+    def run(self, num_steps):
+        for _ in range(num_steps):
+            k = self.run_one_step()
+            self.counts[k] += 1
+            self.action.append(k)
+            self.update_regret(k)
