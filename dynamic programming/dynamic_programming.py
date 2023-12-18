46200e393dbbb6daa236c708f86d92102ab75802
Harrbit <70574504+Harrbit@users.noreply.github.com>
2023-12-18 11:12:02 +0800

    value iteration

--- dynamic programming/dynamic_programming.py
+++ dynamic programming/dynamic_programming.py
@@ -104,11 +104,64 @@ def print_agent(agent, action_meaning, disaster = [], end = []):
                 print(pi_str, end=' ')
             print()
 
-
+'''
 env = CliffWalkingEnv()
 action_meaning = ['^', 'v', '<', '>']
 theta = 0.001
 gamma = 0.9
 agent = PolicyIteration(env, theta, gamma)
 agent.policy_iteration()
+print_agent(agent, action_meaning, list(range(37, 47)), [47])
+'''
+
+class ValueIteration:
+    def __init__(self, env, theta, gamma):
+        self.env = env
+        self.v = [0] * self.env.ncol * self.env.nrow
+        self.theta = theta
+        self.gamma = gamma
+        self.pi = [None for i in range(self.env.ncol * self.env.nrow)]
+
+
+    def value_iteration(self):
+        cnt = 0
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
+                qsa_list = []
+                for a in range(4):
+                    qsa = 0
+                    for res in self.env.P[s][a]:
+                        p, next_state, r, done = res
+                        qsa += p * (r + self.gamma * self.v[next_state] * (1-done))
+                    qsa_list.append(qsa)
+                new_v[s] = max(qsa_list)
+                max_diff = max(max_diff, abs(new_v[s] - self.v[s]))
+            self.v = new_v
+            if max_diff < self.theta:break
+            cnt += 1
+        print("价值迭代一共进行%d轮"%cnt)
+        self.get_policy()
+
+    def get_policy(self):
+        for s in range(self.env.ncol * self.env.nrow):
+            qsa_list = []
+            for a in range(4):
+                qsa = 0
+                for res in self.env.P[s][a]:
+                    p, next_state, r, done = res
+                    qsa += r + p * self.gamma * self.gamma *self.v[next_state] * (1-done)
+                qsa_list.append(qsa)
+            maxq = max(qsa_list)
+            cntq = qsa_list.count(maxq)
+            self.pi[s] = [1/cntq if q == maxq else 0 for q in qsa_list]
+
+
+env = CliffWalkingEnv()
+action_meaning = ['∧', '∨', '<', '>']
+theta = 0.001
+gamma = 0.9
+agent = ValueIteration(env, theta, gamma)
+agent.value_iteration()
 print_agent(agent, action_meaning, list(range(37, 47)), [47])
