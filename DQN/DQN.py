54d60302c4128c0be03888237333b7158e99e2f9
Harrbit <70574504+Harrbit@users.noreply.github.com>
2023-12-20 16:24:51 +0800

    updated DQN

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -118,40 +118,8 @@ action_dim = env.action_space.n
 agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon,
             target_update, device)
 
-return_list = []
-for i in range(10):
-    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
-        for i_episode in range(int(num_episodes / 10)):
-            episode_return = 0
-            state = env.reset()
-            done = False
-            while not done:
-                action = agent.take_action(state)
-                next_state, reward, done, _ = env.step(action)
-                replay_buffer.add(state, action, reward, next_state, done)
-                state = next_state
-                episode_return += reward
-                # 当buffer数据的数量超过一定值后,才进行Q网络训练
-                if replay_buffer.size() > minimal_size:
-                    b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
-                    transition_dict = {
-                        'states': b_s,
-                        'actions': b_a,
-                        'next_states': b_ns,
-                        'rewards': b_r,
-                        'dones': b_d
-                    }
-                    agent.update(transition_dict)
-            return_list.append(episode_return)
-            if (i_episode + 1) % 10 == 0:
-                pbar.set_postfix({
-                    'episode':
-                    '%d' % (num_episodes / 10 * i + i_episode + 1),
-                    'return':
-                    '%.3f' % np.mean(return_list[-10:])
-                })
-            pbar.update(1)
 
+return_list = rl_utils.train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size)
 
 episodes_list = list(range(len(return_list)))
 plt.plot(episodes_list, return_list)
