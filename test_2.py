7030de2406fed0ef8489448ada2c648b66dd0c05
Harrbit <2471691727@qq.com>
2024-02-19 15:46:23 +0800

    mujoco_py working fine

--- test_2.py
+++ test_2.py
@@ -1,19 +1,9 @@
+import mujoco_py
 import os
-
-os.add_dll_directory("C://Users//24716//.mujoco//mujoco200//bin")
-os.add_dll_directory("C://Users//24716//.mujoco//mujoco-py//mujoco_py")
-
-import gym
-env = gym.make("HalfCheetah-v2", render_mode='human')
-# env = gym.wrappers.RecordVideo(env, video_folder='./runs/monitor', name_prefix='mario')
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-for i in range(10):
-
-    obs = env.reset()
-    for t in range(100):
-        action = env.action_space.sample()
-        obs, reward, done, info, _ = env.step(action)
-        if done:
-           break
-env.close()
+mj_path = mujoco_py.utils.discover_mujoco()
+xml_path = os.path.join(mj_path, 'model', 'humanoid.xml')
+model = mujoco_py.load_model_from_path(xml_path)
+sim = mujoco_py.MjSim(model)
+print(sim.data.qpos)
+sim.step()
+print(sim.data.qpos)
